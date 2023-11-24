73402d24fc9e897ec5b7cf9ee98b4171e2b24e6c
Redrogue12 <exgc12@gmail.com>
2023-11-24 15:53:07 -0500

    add home page component

--- src/router.js
+++ src/router.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Modules from './components/Modules.vue'
 import ModuleExercise from './components/ModuleExercise.vue'
 import NotFound from './components/NotFound.vue'
+import HomePage from './components/HomePage.vue'
 
 // https://router.vuejs.org/guide/essentials/dynamic-matching.html
 export const router = createRouter({
@@ -15,6 +16,11 @@ export const router = createRouter({
     {
       path: '/',
       name: 'home',
+      component: HomePage,
+    },
+    {
+      path: '/modules',
+      name: 'modules',
       component: Modules,
     },
     {
